3d62faf69bd4a94be1ace4fdfdca31f5241797eb
codingwebsa <codingwebsa@gmail.com>
2024-03-19 13:16:42 +0600

    feature: blur images

--- src/app/(app)/photos/page.tsx
+++ src/app/(app)/photos/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import * as fs from "fs/promises";
+import Image from "next/image";
+import sharp from "sharp";
+import { getPlaiceholder } from "plaiceholder";
+import * as fs from "node:fs/promises";
+import path from "node:path";
+
 import { cn } from "~/lib/utils";
 
 interface Photo {
@@ -18,13 +23,26 @@ export default async function PhotosPage() {
     <main className="pt-32">
       <div>
         <section className="column-1 gap-x-1.5 px-2 md:columns-2 lg:columns-3">
-          {data.map((p, i) => {
+          {data.map(async (p, i) => {
+            const src = path.join(process.cwd() + `/public/${p.photo_url}`);
+            const buffer = await fs.readFile(src);
+            const { base64 } = await getPlaiceholder(buffer);
+            const metadata = await sharp(src).metadata();
+
             return (
               <div
                 key={`photo${i}`}
                 className={cn("mt-1.5", i === 0 && "mt-0")}
               >
-                <img src={p.photo_url} className="h-auto w-full" alt="" />
+                <Image
+                  src={p.photo_url}
+                  width={metadata.width}
+                  height={metadata.height}
+                  placeholder="blur"
+                  blurDataURL={base64}
+                  className="h-auto w-full"
+                  alt=""
+                />
               </div>
             );
           })}
